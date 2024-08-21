0bdbe99889888a5bd331ea76c413cc6ab69eb088
Théo Laperrouse <theo.laperrouse@energiency.com>
2024-08-21 13:44:39 +0200

    fix: fix reactivity on stravaAccessToken

--- src/composables/useStravaQueries.js
+++ src/composables/useStravaQueries.js
@@ -2,21 +2,23 @@ import { getActivities, getAthleteData } from '@/services/strava.service';
 import { useQuery } from '@tanstack/vue-query';
 
 export const useGetActivities = (accessToken, params = {}) => {
+    const isEnabled = () => !!accessToken.value;
     return useQuery({
         queryKey: ['activities', accessToken, params],
         queryFn: () => getActivities(accessToken.value, params),
         staleTime: 5 * 60 * 1000,
-        enabled: !!accessToken.value,
+        enabled: isEnabled,
         retry: false,
     });
 };
 
 export const useGetAthleteData = (accessToken) => {
+    const isEnabled = () => !!accessToken.value;
     return useQuery({
         queryKey: ['athleteData', accessToken],
         queryFn: () => getAthleteData(accessToken.value),
         staleTime: 24 * 60 * 60 * 1000,
-        enabled: !!accessToken.value,
+        enabled: isEnabled,
         retry: false,
     });
 };
